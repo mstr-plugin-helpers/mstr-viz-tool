ee2b51a5879286d5700c6b231edbcf59841b1340
Paul Bailey <paul.bailey@rinbo.consulting>
2018-04-23 21:14:32 +0100

    Start expanding commands

--- cli.js
+++ cli.js
@@ -2,9 +2,16 @@
 const program = require('commander')
 const templator = require('./src/templator.js')
 const path = require('path')
+const { prompt } = require('inquirer')
+
+program
+  .version('0.0.1')
+  .description('Helper tool for building MicroStrategy visualisations')
 
 program
  .command('init <name> [path]')
+ .alias('i')
+ .description('Creates a new visualisation from templates')
  .action(function(name, pathName) {
    if (!pathName) {
     pathName = process.cwd()
