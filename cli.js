9654f066f92c076cd16271070c065eac93a36c25
Paul Bailey <paul.bailey@rinbo.consulting>
2018-04-24 21:49:47 +0100

    Prompted init

--- cli.js
+++ cli.js
@@ -4,20 +4,36 @@ const templator = require('./src/templator.js')
 const path = require('path')
 const { prompt } = require('inquirer')
 
+const initQuestions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'Enter plugin name...'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter description...'
+  }
+
+]
+
 program
   .version('0.0.1')
   .description('Helper tool for building MicroStrategy visualisations')
 
 program
- .command('init <name> [path]')
- .alias('i')
- .description('Creates a new visualisation from templates')
- .action((name, pathName) => {
-   if (!pathName) {
-    pathName = process.cwd()
-   }
-   const templateDir = path.resolve(__dirname, 'templates')
-   templator.templateDirectory(templateDir, pathName, { 'name': name })
- })
+  .command('init [path]')
+  .alias('i')
+  .description('Creates a new visualisation from templates')
+  .action((pathName) => {
+    if (!pathName) {
+      pathName = process.cwd()
+    }
+    prompt(initQuestions).then(answers => {
+      const templateDir = path.resolve(__dirname, 'templates')
+      templator.templateDirectory(templateDir, pathName, answers)
+    })
+  })
 
- program.parse(process.argv)
+program.parse(process.argv)
